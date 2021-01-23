0693339db5ae67a33014f4b09d78f092692df54b
Ben Wilson <ben@thelocust.org>
2021-01-23 13:39:01 -0500

    update topnav link to benwilson.org

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -5,12 +5,6 @@ var store = [{
         "url": "http://localhost:4000/sbdm/aboutme/why-i-joined-sbdm/",
         "teaser": null
       },{
-        "title": "\"In JCPS there is no legacy\" - Equity vs. Continuity",
-        "excerpt":"After our council selected a new principal, I reached out to a number of folks I knew - some JCPS employees, some not - who had first-hand experience with a change of principal. How did it affect them? How did it affect the school community (parents and staff both)? Is,...","categories": [],
-        "tags": [],
-        "url": "http://localhost:4000/sbdm/in-jcps-there-is-no-legacy-equity-vs-continuity/",
-        "teaser": null
-      },{
         "title": "Meeting Notes: January 19, 2021",
         "excerpt":"Hello LPAS Families and Friends! Below are my notes from the January 19th 2021 LPAS SBDM meeting. Note: I’m one of two parent reprensentatives, so my notes are slanted towards parent/family topics. As far as notes go, they are probably incomplete, and are definitely not “official”. Official agendas and approved...","categories": ["meetingnotes"],
         "tags": [],
